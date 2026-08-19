ea28a1305fff91a409f23b932bdc109eadfa7f06
LAB12_USER ON WS22 <L12X19W22@LabHCI.net>
2026-08-19 20:38:05 +0800

    Create styled_text.dart class

--- lab_act_2/lib/gradient_container.dart
+++ lab_act_2/lib/gradient_container.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:lab_act_2/styled_text.dart';
+
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
           colors: [Colors.blue, Colors.greenAccent],
         ),
       ),
-      child: Center(child: Text('Hello World')),
+      child: const Center(
+        child: StyledText()
+        ),
     );
   }
 }
