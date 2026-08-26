5bf7bd01817078c0fe15412101d16d85461f12ae
LAB12_USER ON WS22 <L12X19W22@LabHCI.net>
2026-08-26 19:28:29 +0800

    Add randomizer

--- lab_act_2/lib/gradient_container.dart
+++ lab_act_2/lib/gradient_container.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lab_act_2/dice_roller.dart';
 import 'styled_text.dart';
 
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+const GradientContainer(this.colors,{super.key});
+final List<Color> colors;
+
+  var currentDiceImage = 'assets/dice-images/dice-3.png';
+  void rollDice(){
+      currentDiceImage = 'assets/dice-images/dice-5.png';
+  }
   @override
   Widget build(context) {
     return Container(
@@ -15,8 +22,9 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Image.asset('assets/dice-images/dice-3.png')
+        child: DiceRoller()
         ),
+
     );
   }
 }
