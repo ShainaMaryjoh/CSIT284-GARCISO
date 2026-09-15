fdff26675e9587aa3765a4837bb80b515921b3e9
Shaina MaryJoh J. Garciso <shainamaryjoh.garciso@cit.edu>
2026-09-15 16:47:31 +0800

    Add quiz questions

--- lab_act_3/lib/gradient_container.dart
+++ lab_act_3/lib/gradient_container.dart
@@ -1,57 +1,97 @@
 import 'package:flutter/material.dart';
-import 'quiz.dart';
-import 'styled_text.dart';
+import 'questions.dart';
 
-
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<GradientContainer> createState() {
+    return _GradientContainerState();
+  }
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+  var quizStarted = false;
+
+  void startQuiz() {
+    setState(() {
+      quizStarted = true;
+    });
+  }
+
+  @override
+  Widget build(context) {
+   if (quizStarted) {
+  return Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          questions[0].question,
+          style: const TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 40),
+        ...questions[0].answers.map(
+          (answer) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 6,
+              ),
+              child: SizedBox(
+                width: 350,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(answer),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    ),
+  );
+}
+
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
           colors: [
-            Colors.green,
-            Colors.brown,
+            Color.fromARGB(255, 30, 83, 2),
+            Color.fromARGB(255, 137, 102, 91),
           ],
         ),
       ),
-     child: Center(
-  child: Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Image.asset(
-        'assets/quiz-logo.png',
-        width: 200,
-      ),
-      const SizedBox(height: 30),
-      const StyledText('Learn Flutter the fun way!'),
-      const SizedBox(height: 30),
-      TextButton(
-       onPressed: () {
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => const Quiz(),
-    ),
-  );
-},
-        child: const Text(
-          'Start Quiz',
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-          ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/quiz-logo.png',
+              width: 250,
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'Test Your Flutter Knowledge!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: startQuiz,
+              child: const Text('Start Quiz'),
+            ),
+          ],
         ),
       ),
-    ],
-  ),
-),
-
-
     );
   }
 }
---
+++ lab_act_3/lib/questions.dart
@@ -0,0 +1,63 @@
+class QuizQuestion {
+  const QuizQuestion(this.question, this.answers);
+
+  final String question;
+  final List<String> answers;
+}
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+    [
+      'Widgets',
+      'Functions',
+      'Components',
+      'Blocks',
+    ],
+  ),
+  QuizQuestion(
+    'How are Flutter UIs built?',
+    [
+      'By combining widgets in code',
+      'By using XCode for iOS and Android Studio for Android',
+      'By combining components in a visual editor',
+      'By defining widgets in config files',
+    ],
+  ),
+  QuizQuestion(
+    "What's the purpose of a StatefulWidget?",
+    [
+      'Update UI as data changes',
+      'Ignore data changes',
+      'Render UI that does not depend on data',
+      'Update data without changing the UI',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'How should you update data inside of StatefulWidgets?',
+    [
+      'By calling setState()',
+      'By calling updateUI()',
+      'By calling updateData()',
+      'By calling updateState()',
+    ],
+  ),
+  QuizQuestion(
+    'What happens if you change data in a StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+];
